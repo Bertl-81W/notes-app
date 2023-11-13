1f721060cc1b955ba0d1dc32a00ac667e000e801
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2023-11-12 23:11:04 -0700

    Initial commit

---
+++ note app/src/firebase.js
@@ -0,0 +1,22 @@
+/*import /*{ initializeApp } */ /*firebase from "firebase/app"; 
+import 'firebase/auth';
+import /*{ getFirestore } from */ /*"firebase/firestore";*/
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_uXh8KfNe11S8qhfQuB_RAzIHrKFTOHE",
+  authDomain: "react-notes-7a013.firebaseapp.com",
+  projectId: "react-notes-7a013",
+  storageBucket: "react-notes-7a013.appspot.com",
+  messagingSenderId: "370854322953",
+  appId: "1:370854322953:web:3e20fb09a3b7f61fc329d8"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const notesCollection = collection(db, "notes")
+
+firebase.initializeApp(firebaseConfig);
